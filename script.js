75453a66b6a13163d8bf0e002c4d2f562f6bebc6
Shovon Chowdhury <88574002+svn-111@users.noreply.github.com>
2024-08-20 15:52:16 +0600

    RemovingBug

--- script.js
+++ script.js
@@ -27,51 +27,47 @@ let turn = true;
 boxes.forEach((box) => 
     box.addEventListener("click", () =>
     {
-        if(name1==="" && name2==="")
-        {
+        if(name1==="" && name2===""){
             statusUpdate.classList.add("invalid-move");
             statusUpdate.innerText="Insert Player Names...";
         }
-        else
-        {
+
+        else{
             statusUpdate.classList.remove("invalid-move");
-            if(turn)
-            {
-            if(box.innerText==="")
-            {
-                hitCount+=1;
-                box.innerText = "X";
-                turn = false;
-                statusUpdate.classList.remove("invalid-move");
-                statusUpdate.innerText = "";
+            if(turn){
+                if(box.innerText==="")
+                {
+                    hitCount+=1;
+                    box.innerText = "X";
+                    turn = false;
+                    statusUpdate.classList.remove("invalid-move");
+                    statusUpdate.innerText = "";
+                }
+                else
+                {
+                    statusUpdate.classList.add("invalid-move");
+                    statusUpdate.innerText = "Invalid Move";
+                }           
             }
-            else
-            {
-                statusUpdate.classList.add("invalid-move");
-                statusUpdate.innerText = "Invalid Move";
-            }           
-            }
-            else
-            {
-            if(box.innerText==="")
-            {
-                hitCount+=1;
-                box.innerText = "O";
-                turn = true;
-                statusUpdate.classList.remove("invalid-move");
-                statusUpdate.innerText = "";
-            }
-            else
-            {
-                statusUpdate.classList.add("invalid-move");
-                statusUpdate.innerText = "Invalid Move";
-            }            
+            else{
+                if(box.innerText==="")
+                {
+                    hitCount+=1;
+                    box.innerText = "O";
+                    turn = true;
+                    statusUpdate.classList.remove("invalid-move");
+                    statusUpdate.innerText = "";
+                }
+                else
+                {
+                    statusUpdate.classList.add("invalid-move");
+                    statusUpdate.innerText = "Invalid Move";
+                }            
             }
             checkWinner();
-            }
+        }
         
     }
-
     )
 )
 
@@ -94,98 +90,88 @@ function checkWinner()
     let win = false;
     let winner = "";
     for(pattern of winPatterns){
-        // console.log((boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText));
-
         if ((boxes[pattern[0]].innerText==="O") && (boxes[pattern[1]].innerText==="O") && (boxes[pattern[2]].innerText === "O"))
         {
             win = true;
             winner = "O";
             cnt2+=1;
-            winner = boxes[pattern[0]].innerText;
             boxes.forEach((box) => {
                 setTimeout(() => {
                     box.innerText="";
                   }, "500");
-            }
+            })
+
+            statusUpdate.innerText = `${name2} wins`;
+            setTimeout(() => {
+                statusUpdate.innerText = "";
+                }, "1500");
+
+            turn = true;
+            hitCount=0;                 
+        } 
             
-        )
-        turn = true;
-            
-        }
         if ((boxes[pattern[0]].innerText==="X") && (boxes[pattern[1]].innerText==="X") && (boxes[pattern[2]].innerText === "X"))
         {
             win = true;
             winner = "X";
             cnt1+=1;
-            winner = boxes[pattern[0]].innerText;
             boxes.forEach((box) => {
                 setTimeout(() => {
                     box.innerText="";
                   }, "500");
             }
         )
-        turn = true;
-        }
-        
-        
-    }
-    if(win === true)
-    {
-        if(winner === "X")
-        {
             statusUpdate.innerText = `${name1} wins`;
             setTimeout(() => {
                 statusUpdate.innerText = "";
-              }, "1500");
-            // statusUpdate.innerText = `${name1} wins`;
-        }            
-        else
-        {
-            statusUpdate.innerText = `${name2} wins`;
-            setTimeout(() => {
-                statusUpdate.innerText = "";
-              }, "1500");
+                }, "1500");  
+            turn = true;
+            hitCount=0;        
         }
-        hitCount=0;
-            
-    }
-    else
-    {
-        if(hitCount===9)
+        if(win === false && hitCount===9)
         {
             cntd+=1;
             statusUpdate.innerText="Match Draw";
             setTimeout(() => {
                 statusUpdate.innerText = "";
-              }, "1500");
+                }, "1500");
             console.log(hitCount);
             boxes.forEach((box) => {
                 setTimeout(() => {
                     box.innerText="";
-                  }, "1000");
+                    }, "1000");
             }
         )
-        hitCount=0;
+            turn = true;
+            hitCount=0;
+     
         }
-            
+        winner = "";
+        win = false;   
     }
     score();
 }
+
 function resetAll(){
     boxes.forEach((box) =>
     {
         box.innerText = ""
     }
     );
-    //console.log(boxes.innerText);
     turn = true;
-    // statusUpdate.innerText = "Status :";
+    hitCount=0; 
+    winner = "";
+    win = false;
+    cnt1=0;
+    cnt2=0;
+    cntd=0;   
     player1Name.value="";
     player2Name.value="";
     name1="";
     name2="";
     Scores.innerText="Scores : "
 }
+
 function score()
 {
     Scores.innerText = `Score : ${name1} = ${cnt1} || ${name2} = ${cnt2} || Draw = ${cntd}`
